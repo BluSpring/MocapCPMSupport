7125bba8e9192c69c470267be1ab18fd472a07f4
Naz <bluspringindohq@gmail.com>
2025-03-04 00:37:38 +0800

    Remove unused mixins and lax the dependencies

--- src/main/java/xyz/bluspring/mocapcpmsupport/mixin/ActionTypeMixin.java
+++
@@ -1,8 +0,0 @@
-package xyz.bluspring.mocapcpmsupport.mixin;
-
-import com.mt1006.mocap.mocap.actions.Action;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Action.Type.class)
-public class ActionTypeMixin {
-}
